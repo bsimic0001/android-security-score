193dfa8da2eda555593076ff22ab05ab341fb3f3
Bojan Simic <bsimic0001@gmail.com>
2013-08-15 15:52:16 -0400

    Revert "removing email notification."
    This reverts commit 4877e88e05f31bf8c722141ae168490c45f0ae4a.

--- AndroidSecurityAnalytics/src/com/device/security/analytics/androidsecurityanalytics/FrontPageActivity.java
+++ AndroidSecurityAnalytics/src/com/device/security/analytics/androidsecurityanalytics/FrontPageActivity.java
@@ -41,10 +41,12 @@ public class FrontPageActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		//Thread.setDefaultUncaughtExceptionHandler(new TopExceptionHandler(this));
-		//sendExistingStack();
+		Thread.setDefaultUncaughtExceptionHandler(new TopExceptionHandler(this));
+
+		sendExistingStack();
 
 		setContentView(R.layout.front_page);
+
 		new CalculateScoreTask(this, "Calculating score...").execute();
 		// doMainActivity();
 	}
